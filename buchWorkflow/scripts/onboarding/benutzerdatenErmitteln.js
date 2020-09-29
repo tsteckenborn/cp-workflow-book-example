927d6ea115f768de11e11bffcc9c505e7a1c509e
tsteckenborn <50519619+tsteckenborn@users.noreply.github.com>
2020-09-29 17:16:11 +0200

    Update benutzerdatenErmitteln.js

--- buchWorkflow/scripts/onboarding/benutzerdatenErmitteln.js
+++ buchWorkflow/scripts/onboarding/benutzerdatenErmitteln.js
@@ -1,20 +1,18 @@
 // Simulieren der Antwort von Microsoft Graph
-var benutzer = {
-	"@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users/$entity",
-	"businessPhones": [
-		"+49 6232 1212201"
+var benutzer = { "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users/$entity",
+"businessPhones": [
+"+49 1111 1111111"
 	],
-	"displayName": "Albert Steckenborn",
-	"givenName": "Albert",
-	"jobTitle": "Geschäftsführer",
-	"mail": "albert.steckenborn@consolvis.de",
-	"mobilePhone": "+49 171 1206180",
-	"officeLocation": null,
-	"preferredLanguage": "de-DE",
-	"surname": "Steckenborn",
-	"userPrincipalName": "albert.steckenborn@consolvis.de",
-	"id": "9e4318c0-6ee1-4928-82fe-a04113f4ef79"
-};
+"displayName": "Vorname Nachname",
+"givenName": "Vorname",
+"jobTitle": "Titel",
+"mail": "mail@host.de",
+"mobilePhone": "+49 111 1111111",
+"officeLocation": null,
+"preferredLanguage": "de-DE",
+"surname": "Nachname",
+"userPrincipalName": "mail@host.de",
+"id": "1e1111c1-1ee1-1111-11fe-a11111f1ef11"};
 
 // Schreiben des 'benutzer' Knotens in den Workflow-Kontext
 $.context.benutzer = benutzer;
